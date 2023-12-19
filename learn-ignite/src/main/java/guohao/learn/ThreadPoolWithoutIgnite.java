a393e1ea8eeda27ddc950a78b13dfbc50a9bed75
guohao <guohao02@corp.netease.com>
2023-12-19 20:46:46 +0800

    feat: ignite
    feat: build
    
    feat: ignite

---
+++ learn-ignite/src/main/java/guohao/learn/ThreadPoolWithoutIgnite.java
@@ -0,0 +1,22 @@
+package guohao.learn;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+public class ThreadPoolWithoutIgnite {
+    public static void main(String[] args) throws InterruptedException {
+        ExecutorService service = Executors.newFixedThreadPool(16);
+
+        long start = System.currentTimeMillis();
+
+        for (int i = 1; i <= 50; i++) {
+            service.submit(new MyRunnable(i));
+        }
+
+        service.shutdown();
+        service.awaitTermination(1, TimeUnit.MINUTES);
+
+        System.out.printf("Time taken %dms.%n", (System.currentTimeMillis() - start));
+    }
+}
