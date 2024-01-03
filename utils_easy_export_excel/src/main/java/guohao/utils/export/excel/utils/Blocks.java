12b87b3146e9eb8c566a3a438b59e7521521e6dc
Hao Guo <guohao01@rd.netease.com>
2024-01-03 23:25:03 +0800

    feat: blocks

---
+++ utils_easy_export_excel/src/main/java/guohao/utils/export/excel/utils/Blocks.java
@@ -0,0 +1,61 @@
+package guohao.utils.export.excel.utils;
+
+import lombok.Getter;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Blocks {
+    private final List<Block> blocks = new ArrayList<>();
+
+    private int maxRow = 0;
+    private int sumCol = 0;
+
+    public Blocks addBlock(Object[][] block) {
+        Block newBlock = new Block(block);
+        blocks.add(newBlock);
+        this.maxRow = Math.max(this.maxRow, newBlock.getRowNum());
+        this.sumCol += newBlock.getColNum();
+        return this;
+    }
+
+    public List<List<Object>> mergeToList() {
+        return Arrays.stream(merge())
+                .map(Arrays::asList)
+                .collect(Collectors.toList());
+    }
+
+    public Object[][] merge() {
+        Object[][] result = new Object[this.maxRow][this.sumCol];
+        int curCol = 0;
+        for (Block block : blocks) {
+            block.copy(curCol, result);
+            curCol += block.getColNum();
+        }
+        return result;
+    }
+
+    @Getter
+    public static class Block {
+        private final Object[][] block;
+        private int colNum = 0;
+        private int rowNum = 0;
+
+        public Block(Object[][] block) {
+            this.block = block;
+            this.rowNum = block.length;
+            for (Object[] objects : block) {
+                this.colNum = Math.max(this.colNum, objects.length);
+            }
+        }
+
+        public void copy(int startCol, Object[][] matrix) {
+            for (int i = 0; i < this.block.length; i++) {
+                System.arraycopy(block[i], 0, matrix[i], startCol,block[i].length);
+            }
+        }
+    }
+
+}
---
+++ utils_easy_export_excel/src/test/java/BlocksTest.java
@@ -0,0 +1,37 @@
+import com.alibaba.excel.support.cglib.core.Block;
+import guohao.utils.export.excel.utils.Blocks;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class BlocksTest {
+
+    @Test
+    void test() {
+        Blocks blocks = new Blocks();
+        blocks.addBlock(new Object[][]{
+                        {null}
+                })
+                .addBlock(new Object[][]{
+                        {1}
+                })
+                .addBlock(new Object[][]{
+                        {2, "2"},
+                        {2, "2"}
+                })
+                .addBlock(new Object[][]{
+                        {3, "3", "3"},
+                        {3, "3", "3"},
+                        {3, "3", "3"}
+                })
+                .addBlock(new Object[][]{
+                        {4, "4"},
+                        {4, "4"}
+                });
+        Object[][] result = {
+                {null, 1, 2, "2", 3, "3", "3", 4, "4"},
+                {null, null, 2, "2", 3, "3", "3", 4, "4"},
+                {null, null, null, null, 3, "3", "3", null, null}
+        };
+        Assertions.assertArrayEquals(result, blocks.merge());
+    }
+}
